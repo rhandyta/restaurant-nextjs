9612881dc68907668c8ce085738d124083ff55b9
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-03-29 23:33:25 +0700

    first

---
+++ app/auth/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LayoutAuth({ children }) {
+    return (
+        <>
+            <h1>Layout auth</h1>
+            {children}
+        </>
+    );
+}
+
+export default LayoutAuth;
