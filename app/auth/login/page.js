b7eb4a568caf18d7c5dfc4759a207dacd995998c
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-04-07 17:19:29 +0700

    login

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -10,25 +10,34 @@ function Login() {
     return (
         <section className="h-96 md:h-auto">
             <h6 className="font-semibold text-4xl text-center">Login</h6>
-            <form className="flex flex-wrap">
-                <div className="form-control w-full">
-                    <Label label="Email" />
-                    <input
-                        type="text"
-                        placeholder="Input your email"
-                        className="input input-bordered w-full"
-                    />
-                </div>
-                <div className="form-control w-full">
-                    <Label label="Password" />
-                    <input
-                        type="password"
-                        placeholder="Input your password"
-                        className="input input-bordered w-full"
-                    />
-                </div>
-                <Button text="Login" className="mt-5 btn-block btn-sm" />
-            </form>
+            <Formik>
+                {(props) => {
+                    return (
+                        <Form className="flex flex-wrap">
+                            <div className="form-control w-full">
+                                <Label label="Email" />
+                                <Input
+                                    type="text"
+                                    placeholder="Input your email"
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
+                            <div className="form-control w-full">
+                                <Label label="Password" />
+                                <Input
+                                    type="password"
+                                    placeholder="Input your password"
+                                    className="input input-bordered w-full"
+                                />
+                            </div>
+                            <Button
+                                text="Login"
+                                className="mt-5 btn-block btn-sm"
+                            />
+                        </Form>
+                    );
+                }}
+            </Formik>
             <p className="mt-3 inline-block font-thin text-sm">
                 don't have an account?{" "}
                 <span className="hover:underline hover:text-blue-400">
